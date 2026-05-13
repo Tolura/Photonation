e11d6a2c468bd8c415c19077e9bf4e343b834be6
Tharn <88618198+Tolura@users.noreply.github.com>
2026-05-13 03:33:43 +0200

    Update usage.lua

--- OffsetAPI/usage.lua
+++ OffsetAPI/usage.lua
@@ -1,12 +1,10 @@
 --[[
 API Usage Examples:
-1) Print Specific Offset
--- Example: Print the value of a specific offset
-print(HealthOffset)
+Print Specific Offset Example: Print the value of a specific offset
+print(Humanoid_HealthOffset)
 
 
-2) Print All Offsets
--- Example: Loop through all global variables ending with "Offset"
+Print All Offsets Example: Loop through all global variables ending with "Offset"
 for k, v in pairs(_G) do
     if k:match("Offset$") then
         print(k .. " = " .. tostring(v))
