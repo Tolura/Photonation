b05dd1d905bdf544e3e12d6e092a2c7665a98a5e
Tharn <88618198+Tolura@users.noreply.github.com>
2026-05-13 03:32:23 +0200

    Update offset.lua

--- OffsetAPI/offset.lua
+++ OffsetAPI/offset.lua
@@ -7,8 +7,20 @@ hook.add("init", "offset_create", function()
     http.get(Provider, function(body, status)
         if status ~= 200 or not body then return end
 
-        for Offset, Hex in body:gmatch("inline%s+constexpr%s+uintptr_t%s+([%w_]+)%s*=%s*(0x[%x]+)") do
-            _G[Offset .. "Offset"] = tonumber(Hex)
+        local Namespace = ""
+        
+        for line in body:gmatch("[^\r\n]+") do
+            local InNamespace = line:match("namespace%s+([%w_]+)%s*{")
+            if InNamespace then
+                Namespace = InNamespace .. "_"
+            elseif line:match("^%s*}") and Namespace ~= "" then
+                Namespace = ""
+            end
+            
+            local Offset, Hex = line:match("inline%s+constexpr%s+uintptr_t%s+([%w_]+)%s*=%s*(0x[%x]+)")
+            if Offset then
+                _G[Namespace .. Offset .. "Offset"] = tonumber(Hex)
+            end
         end
 
         hook.remove("init", "offset_create")
